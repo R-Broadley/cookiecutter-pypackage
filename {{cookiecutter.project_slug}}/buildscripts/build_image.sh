bb6623eaf93d4bd428182afb9cf0091eb5d5ad58
Rob Broadley <git@rbroadley.co.uk>
2020-07-13 17:59:47 +0100

    Improve setting of container user
    Fixes warning on building the image.

--- {{cookiecutter.project_slug}}/buildscripts/build_image.sh
+++ {{cookiecutter.project_slug}}/buildscripts/build_image.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+BUILDSCRIPTS="$(dirname "$0")"
+source "$BUILDSCRIPTS/set_container_user_var.sh"
+
 docker-compose build $1
--- {{cookiecutter.project_slug}}/buildscripts/start_container.sh
+++ {{cookiecutter.project_slug}}/buildscripts/start_container.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-function run_as_user {
-		CONTAINER_USER="$(id -u):$(id -g)" \
-		&& export CONTAINER_USER \
-		&& docker-compose run --rm $@
-}
+BUILDSCRIPTS="$(dirname "$0")"
+source "$BUILDSCRIPTS/set_container_user_var.sh"
 
-case $OSTYPE in
-  *linux*) run_as_user $@ ;;
-  *)       docker-compose run --rm $@ ;;
-esac
+docker-compose run --rm $@
