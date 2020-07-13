932a6a509fb283d5f72909d70c5e6ab7d1dcc38f
Rob Broadley <git@rbroadley.co.uk>
2020-07-13 17:06:36 +0100

    Convert version test to use pytest

--- {{cookiecutter.project_slug}}/unittests/test_version.py
+++ {{cookiecutter.project_slug}}/unittests/test_version.py
@@ -1,69 +1,56 @@
 """Tests for `{{ cookiecutter.project_slug }}` package."""
 
 from distutils.version import StrictVersion
-from functools import partialmethod
-from unittest import TestCase
+from os import getcwd
+from os.path import relpath
+
+import pytest
 
 import {{ cookiecutter.project_slug }}
 
 
-class TestVersionInfo(TestCase):
-    """Tests of package metadata format and consistency."""
+# pylint: disable=protected-access
+@pytest.mark.parametrize(
+    "module",
+    [{{ cookiecutter.project_slug }}, {{ cookiecutter.project_slug }}._version],
+)
+def test_version(module):
+    """Check __version__ is a valid version number.
 
-    def check_version_format(self, version, file):
-        """Check string is a valid version number.
+    Args:
+        module (types.ModuleType): A module containing __version__
 
-        Args:
-            version (str): The version string to check.
-            file (str): the file which contains the version string.
+    """
+    version = module.__version__
+    full_path = module.__file__
+    relative_path = relpath(full_path, start=getcwd())
+    try:
+        StrictVersion(version)
+    except ValueError as exception:
+        pytest.fail(msg=f"{exception} in {relative_path}")
 
-        """
-        try:
-            StrictVersion(version)
-        except ValueError as exception:
-            self.fail(msg=f"{exception} in {file}")
 
+def test_versions_match():
+    """Ensure version in __init__ and _version match."""
+    ver1 = {{ cookiecutter.project_slug }}.__version__
     # pylint: disable=protected-access
-    test_versionpy_version_format = partialmethod(
-        check_version_format,
-        {{ cookiecutter.project_slug }}._version.__version__,
-        "package _version.py",
-    )
+    ver2 = {{ cookiecutter.project_slug }}._version.__version__
+    assert ver1 == ver2, "Version in __init__.py does not match _version.py"
 
-    test_init_version_format = partialmethod(
-        check_version_format,
-        {{ cookiecutter.project_slug }}.__version__,
-        "package __init__.py",
-    )
 
-    def test_versions_match(self):
-        """Ensure version in __init__ and _version match."""
-        self.assertEqual(
-            {{ cookiecutter.project_slug }}._version.__version__,
-            {{ cookiecutter.project_slug }}.__version__,
-            msg="Version in __init__.py does not match _version.py",
+def test_version_is_extractable():
+    """Ensure version number can be extracted without importing package."""
+    version_file = {{ cookiecutter.project_slug }}._version.__file__
+    try:
+        with open(version_file, "r") as vfile:
+            value = vfile.readlines()[-1]
+            version = value.split("=")[1].strip().strip('"')
+    except Exception as exception:  # pylint: disable=broad-except
+        pytest.fail(
+            msg=f"Could not extract version from _version.py due to: `{exception}`"
         )
-
-    # pylint: enable=protected-access
-
-    def test_version_is_extractable(self):
-        """Ensure version number can be extracted without importing package."""
-        version_file = "{{ cookiecutter.project_slug }}/_version.py"
-        try:
-            with open(version_file, "r") as vfile:
-                value = vfile.readlines()[-1]
-                version = value.split("=")[1].strip().strip('"')
-        except Exception as exception:  # pylint: disable=broad-except
-            self.fail(
-                msg=f"Could not extract version from _version.py due to: `{exception}`"
-            )
-
-        self.check_version_format(version, "_version.py (text extraction)")
-        self.assertEqual(
-            {{ cookiecutter.project_slug }}.__version__,
-            version,
-            msg=(
-                "Version number imported from _version.py does not match that "
-                "read from said file."
-            ),
-        )
+    msg = (
+        "Version number imported from _version.py does not match that read from said "
+        "file."
+    )
+    assert {{ cookiecutter.project_slug }}.__version__ == version, msg
